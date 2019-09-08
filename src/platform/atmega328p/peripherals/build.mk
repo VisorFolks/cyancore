a0be91b95ebb07addb3c70cf1323c4036c0f4915
Akash Kollipara <akashkollipara@gmail.com>
2019-09-08 23:35:00 +0530

    <ATMega328p> Added initial support for peripherals

---
+++ src/platform/atmega328p/peripherals/build.mk
@@ -0,0 +1,11 @@
+PERIPHERALS_DIR	:= $(GET_PATH)
+
+include $(PERIPHERALS_DIR)/adc/build.mk
+include $(PERIPHERALS_DIR)/eeprom/build.mk
+include $(PERIPHERALS_DIR)/i2c/build.mk
+include $(PERIPHERALS_DIR)/int/build.mk
+include $(PERIPHERALS_DIR)/pcint/build.mk
+include $(PERIPHERALS_DIR)/spi/build.mk
+include $(PERIPHERALS_DIR)/timer/build.mk
+include $(PERIPHERALS_DIR)/uart/build.mk
+include $(PERIPHERALS_DIR)/wdt/build.mk
