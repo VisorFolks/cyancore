9fc0bc687cec228820cef0febcbda2f8ec2e6458
Akash Kollipara <akashkollipara@gmail.com>
2019-09-09 21:02:45 +0530

    <Make> Added safety checks

--- src/arch/build.mk
+++ src/arch/build.mk
@@ -1,3 +1,13 @@
 ARCH_DIR	:= $(GET_PATH)
 
+ifeq ($(ARCH),)
+$(info < ! > CPU - Architecture is not defined !)
+$(error < x > Build Failed !)
+endif
+
+ifeq ($(BIT),)
+$(info < ! > CPU - Data Width is not defined !)
+$(error < x > Build Failed !)
+endif
+
 include $(ARCH_DIR)/$(ARCH)/$(BIT)/build.mk
--- src/platform/build.mk
+++ src/platform/build.mk
@@ -1,3 +1,8 @@
 PLAT_DIR	:= $(GET_PATH)
 
+ifeq ($(PLATFORM),)
+$(info < ! > Platform for project is not defined !)
+$(error < x > Build Failed !)
+endif
+
 include $(PLAT_DIR)/$(PLATFORM)/build.mk
