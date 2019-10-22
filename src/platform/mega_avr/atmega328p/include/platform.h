a310a12ada2971b81a3ee2e0f2d02802ba673a45
Akash Kollipara <akashkollipara@gmail.com>
2019-10-22 22:15:39 +0530

    <LD> Updated linker script

--- src/platform/mega_avr/atmega328p/include/platform.h
+++ src/platform/mega_avr/atmega328p/include/platform.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <atmega328p.h>
 
-#define PROG_MEM	0X00000000
-#define DATA_MEM	0x00800100
-#define IMEM_START	0x00000000
-#define IMEM_LENGTH	0x4000
-#define DMEM_START	0x00000000
+#define V_IMEM_START	0x00000000
+#define V_DMEM_START	0x00800100
+
+#define L_MEM_START	0x00000000
+#define L_MEM_LENGTH	0x00008000
+
+#define IMEM_LENGTH	0x7800
 #define DMEM_LENGTH	0x0800
-#define STACK_SIZE	0x200
+
+#define STACK_SIZE	0x0200
 
 #define N_ADC_MODULES	1
