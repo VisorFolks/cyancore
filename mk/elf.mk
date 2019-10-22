f20b3b92115c957b6ecbef2dc5b4d030bc3bf26b
Akash Kollipara <akashkollipara@gmail.com>
2019-10-22 23:13:58 +0530

    <Make> Removed additional code to generate bin file

--- mk/elf.mk
+++ mk/elf.mk
@@ -6,11 +6,8 @@ include mk/obj.mk
 
 OBJ_EXE	:= $(addprefix $(OUT)/,$(ELF:.elf=.o))
 ELF	:= $(addprefix $(OUT)/,$(ELF))
-BIN	:= $(subst .elf,.bin,$(ELF))
 
-.PHONY: elf $(BIN)
-
-elf: lib slib $(ELF) $(BIN)
+elf: lib slib $(ELF)
 
 $(ELF): $(DEPS_OBJS) $(OBJ_EXE)
 	echo "Generating $(notdir $@) ..."
@@ -18,14 +15,9 @@ $(ELF): $(DEPS_OBJS) $(OBJ_EXE)
 	$(LD) -T $(LD_SCRIPT) $(LD_FLAGS) -o $@ $^ $(DEP_LIB_PATH) $(DEP_LIBS) -Map=$(subst .elf,.map,$@)
 	echo "Disassembling $(notdir $@) ..."
 	$(OD) -Dx $@ > $(subst .elf,.dis,$@)
+	$(OC) -O binary $@ $(subst .elf,.bin,$@)
+	$(OC) -O ihex $@ $(subst .elf,.hex,$@)
 	echo "=================================================="
 	echo "Size of Executable:"
 	cd $(OUT); $(SIZE) *.elf
 	echo ""
-
-ifeq ($(GENERATE_BIN),1)
-$(BIN): $(ELF)
-	echo "Generating $(notdir $(BIN)) ..."
-	$(OC) -O binary $^ $@
-	$(OC) -O ihex $^ $@
-endif
