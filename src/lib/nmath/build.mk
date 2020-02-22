a1c44e5597a205f4feb7aff5d5973347734707d9
Akash Kollipara <akashkollipara@gmail.com>
2020-02-22 23:39:33 +0530

    <nmath> make update

--- src/lib/nmath/build.mk
+++ src/lib/nmath/build.mk
@@ -1,9 +1,9 @@
-DIR		:= $(GET_PATH)
+NMATH_PATH	:= $(GET_PATH)
 LIB_OBJS	:=
 
 LIB		:= libnmath.a
-LIB_INCLUDE_PATH:= $(DIR)/include
-LIB_HEADER	:= nmath.h
+LIB_INCLUDE	+= -I $(NMATH_PATH)/include/
 DEP_LIBS	+= -lnmath
 
+DIR		:= $(NMATH_PATH)
 include mk/lib.mk
