7c2c49582e9e43cb73c05e6dbe9ded14dba56aff
Akash Kollipara <akashkollipara@gmail.com>
2020-02-23 22:47:15 +0530

    <ATMega328P> Cleared platform functions

--- src/platform/mega_avr/atmega328p/include/platform.h
+++ src/platform/mega_avr/atmega328p/include/platform.h
@@ -12,4 +12,4 @@
 
 #define STACK_SIZE	0x0200
 
-#define N_ADC_MODULES	1
+#define N_CORES		1
--- src/platform/mega_avr/atmega328p/platform/platform.c
+++ src/platform/mega_avr/atmega328p/platform/platform.c
@@ -1,23 +1,14 @@
-#include <platform_api.h>
-#include <shi.h>
-
-SYSTEM_HARDWARE_INFO;
-
 void platform_early_setup()
 {
-	shi config =
-	{
-		.architecture		= ARCHITECTURE,
-		.datawidth		= BITWIDTH,
-//		.n_adc			= 1,
-//		.adc_base_addr[0]	= ADCL,
-//		.adc_interrupt_id[0]	= 22,
-	};
-	UPDATE_SHI(config);
 	return;
 }
 
 void platform_setup()
+{
+	return;
+}
+
+void platform_cpu_setup()
 {
 	return;
 }
