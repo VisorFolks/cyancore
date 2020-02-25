4d66db2f6d2d366550c2c848c6551b4af4c57cee
Akash Kollipara <akashkollipara@gmail.com>
2020-02-26 00:37:59 +0530

    <libarcc> Add rand function

---
+++ src/lib/libarcc/include/arc/rand.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void srand(unsigned long);
+unsigned long rand();
---
+++ src/lib/libarcc/rand.c
@@ -0,0 +1,15 @@
+#include <arc/stdint.h>
+#include <arc/rand.h>
+
+unsigned long randseed = 0x;
+
+void srand(unsigned long seed)
+{
+	randseed = seed;
+}
+
+unsigned long rand()
+{
+	randseed = (randseed * 0x53420986) + 0x23469248;
+	return randseed;
+}
