d6e0b17d69b2d52a2f891eb138c3c0835777a052
Akash Kollipara <akashkollipara@gmail.com>
2020-03-03 22:53:30 +0530

    <Driver> removing redundant code

--- src/driver/build.mk
+++ src/driver/build.mk
@@ -1,14 +1,8 @@
 DRIVER_PATH	:= $(GET_PATH)
 
 include $(DRIVER_PATH)/console/build.mk
+include $(DRIVER_PATH)/chipset/$(FAMILY)/build.mk
 
-ifeq ($(ADC),1)
-include $(DRIVER_PATH)/adc/build.mk
-endif
-
-ifeq ($(UART),1)
-include $(DRIVER_PATH)/uart/build.mk
-endif
 
 DIR		:=$(DRIVER_PATH)
 include mk/obj.mk
