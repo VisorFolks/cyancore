1855fe36cfa1b26257ebaed3a51e61664b1296eb
Akash Kollipara <akashkollipara@gmail.com>
2020-03-14 09:03:51 +0530

    <TC Make> update for auto search of toolchain

--- mk/tc.mk
+++ mk/tc.mk
@@ -1,18 +1,18 @@
 include mk/path.mk
 
-ifeq ($(ARCH),arm-v7)
+ifeq ($(findstring arm-v7,$(ARCH)),arm-v7)
 TC	:= $(TOOLS_ROOT)/arm-toolchain/bin/arm-none-eabi
 TI	:= $(TOOLS_ROOT)/arm-toolchain/arm-none-eabi/include
 TL	:= $(TOOLS_ROOT)/arm-toolchain/arm-none-eabi/lib
 endif
 
-ifeq ($(ARCH),riscv)
+ifeq ($(findstring riscv,$(ARCH)),riscv)
 TC	:= $(TOOLS_ROOT)/risc-v-toolchain/bin/riscv64-unknown-elf
 TI	:= $(TOOLS_ROOT)/risc-v-toolchain/riscv64-unknown-elf/include
 TL	:= $(TOOLS_ROOT)/risc-v-toolchain/riscv64-unknown-elf/lib
 endif
 
-ifeq ($(ARCH),avr5)
+ifeq ($(findstring avr,$(ARCH)),avr)
 TC	:= $(TOOLS_ROOT)/avr-toolchain/bin/avr
 TI	:= $(TOOLS_ROOT)/avr-toolchain/lib/gcc/avr/5.4.0/include-fixed
 TL	:= $(TOOLS_ROOT)/avr-toolchain/avr/lib
