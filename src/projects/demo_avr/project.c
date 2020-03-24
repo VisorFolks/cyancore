2213e58edf2e1c5095525074ae2c247f1e4eaea1
Akash Kollipara <akashkollipara@gmail.com>
2020-03-24 22:29:08 +0530

    <Demo-AVR> Enter password demo program

--- src/projects/demo_avr/project.c
+++ src/projects/demo_avr/project.c
@@ -9,14 +9,39 @@ void project_setup()
 {
 	gpio_pin_config(0, 5, out);
 	gpio_pin_clear(0, 5);
-	console_puts("Type Something! \r\n");
+	printf("Demo Program!\n");
 	return;
 }
 
 void project_loop()
 {
-	char c = getchar();
-	printf("You Entered: %c\n", c);
-	gpio_pin_toggle(0, 5);
+	char c, arr[20];
+	uint8_t i;
+	printf("Enter Password: ");
+	i = 0;
+	do
+	{
+		c = getch();
+		gpio_pin_toggle(0, 5);
+		if(c == '\b')
+		{
+			printf("\b \b");
+			i--;
+		}
+		else if(c != '\r')
+		{
+			printf("*");
+			arr[i++] = c;
+		}
+		
+		if(i > 18)
+		{
+			i = 19;
+			break;
+		}
+	}
+	while(c != '\r');
+	arr[i] = '\0';
+	printf("\nYou Entered: %s\n", arr);
 	return;
 }
