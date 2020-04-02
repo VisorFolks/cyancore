74ae8c3a971bf059cd2270379735b5609e0fc0d8
Akash Kollipara <akashkollipara@gmail.com>
2020-04-02 16:13:03 +0530

    <make> minor fix to provide TC prefix from cli

--- mk/tc.mk
+++ mk/tc.mk
@@ -1,19 +1,19 @@
 include mk/path.mk
 
 ifeq ($(findstring arm-v7,$(ARCH)),arm)
-TC	:= $(TOOLS_ROOT)/arm-toolchain/bin/arm-none-eabi
+TC	?= $(TOOLS_ROOT)/arm-toolchain/bin/arm-none-eabi
 TI	:= $(TOOLS_ROOT)/arm-toolchain/arm-none-eabi/include
 TL	:= $(TOOLS_ROOT)/arm-toolchain/arm-none-eabi/lib
 endif
 
 ifeq ($(findstring riscv,$(ARCH)),riscv)
-TC	:= $(TOOLS_ROOT)/risc-v-toolchain/bin/riscv64-unknown-elf
+TC	?= $(TOOLS_ROOT)/risc-v-toolchain/bin/riscv64-unknown-elf
 TI	:= $(TOOLS_ROOT)/risc-v-toolchain/riscv64-unknown-elf/include
 TL	:= $(TOOLS_ROOT)/risc-v-toolchain/riscv64-unknown-elf/lib
 endif
 
 ifeq ($(findstring avr,$(ARCH)),avr)
-TC	:= $(TOOLS_ROOT)/avr-toolchain/bin/avr
+TC	?= $(TOOLS_ROOT)/avr-toolchain/bin/avr
 TI	:= $(TOOLS_ROOT)/avr-toolchain/lib/gcc/avr/5.4.0/include-fixed
 TL	:= $(TOOLS_ROOT)/avr-toolchain/avr/lib
 endif
