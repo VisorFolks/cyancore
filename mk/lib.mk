29f717a9bd28c95539387f45c01d5914859d0420
Akash Kollipara <akashkollipara@gmail.com>
2020-05-23 15:39:51 +0530

    <make> lib make removed unused vars

--- mk/lib.mk
+++ mk/lib.mk
@@ -8,11 +8,10 @@ LIB		:= $(addprefix $(OUT)/$(DIR)/,$(LIB))
 DEP_LIB_PATH	+= -L $(OUT)/$(DIR)
 DEP_LIBS	+= $(LIB)
 
-$(LIB): $(HEADER) $(LIB_OBJS)
+$(LIB): $(LIB_OBJS)
 	@echo "Generating $(notdir $@) ..."
 	$(AR) rc $@ $^
 
 LIB_INCLUDE_PATH:=
-LIB_HEADER	:=
 LIB_OBJS	:=
 LIB		:=
