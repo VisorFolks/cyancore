fb473fc78227a7ce22cb1fec18c20bddb663a334
Akash Kollipara <akashkollipara@gmail.com>
2020-06-03 06:30:04 +0530

    <TerraVisor> Added services directory for upward compatibilty

--- src/visor/terravisor/build.mk
+++ src/visor/terravisor/build.mk
@@ -3,5 +3,5 @@ TERRA_PATH	:= $(GET_PATH)
 TERRAVISOR	:= 1
 $(eval $(call add_define,TERRAVISOR))
 
-include $(TERRA_PATH)/bootloader/build.mk
+include $(TERRA_PATH)/services/build.mk
 include $(TERRA_PATH)/workers/build.mk
