efbb62aac07cf84db9b7ef23203548f30eda7ec9
Akash Kollipara <akashkollipara@gmail.com>
2020-07-06 18:31:13 +0530

    <TerraKern> Add TerraVisor kernel initial support for providing multi-tasking support on platforms that dont support higher exception levels * bootloader is necessary, but kernel is optional
    TODO:
    * A programmer may choose to use terrakern or can opt out
    * workers will compile irrespective of services

--- src/visor/terravisor/services/build.mk
+++ src/visor/terravisor/services/build.mk
@@ -1,3 +1,10 @@
-DIR		:= $(GET_PATH)
+T_SERVICES	:= $(GET_PATH)
 
+include $(T_SERVICES)/bootloader/build.mk
+
+ifeq ($(TERRAKERN),1)
+include $(T_SERVICES)/kernel/build.mk
+endif
+
+DIR		:= $(T_SERVICES)
 include mk/obj.mk
