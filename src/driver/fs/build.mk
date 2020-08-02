0b449c8cb606a4e044cb2218a6c6654bc5b36ba2
Akash Kollipara <akashkollipara@gmail.com>
2020-08-02 15:36:01 +0530

    <FlexFS> Add support for native fs

---
+++ src/driver/fs/build.mk
@@ -0,0 +1,8 @@
+FS_DIR		:= $(GET_PATH)
+
+#==================================================
+# Mandatory file system for cyancore
+# this needs to be enabled if other fs are
+# being used
+include $(GET_PATH)/flexfs/build.mk
+#==================================================
