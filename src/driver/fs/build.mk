f2527bc8056f14db4cac2fc6949cb3c64c037059
Akash Kollipara <akashkollipara@gmail.com>
2021-01-07 11:44:37 +0530

    Driver directory clean up * Retaining visor(s) as placeholders

--- src/driver/fs/build.mk
+++
@@ -1,8 +0,0 @@
-FS_DIR		:= $(GET_PATH)
-
-#==================================================
-# Mandatory file system for cyancore
-# this needs to be enabled if other fs are
-# being used
-include $(GET_PATH)/flexfs/build.mk
-#==================================================
