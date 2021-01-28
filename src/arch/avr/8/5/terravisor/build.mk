e4000930e9e96e1c4026be458a0dc129433badec
Akash Kollipara <akashkollipara@gmail.com>
2021-01-28 15:34:50 +0530

    <Arch> updated build script file header

--- src/arch/avr/8/5/terravisor/build.mk
+++ src/arch/avr/8/5/terravisor/build.mk
@@ -1,3 +1,9 @@
+#
+# File Name		: build.mk
+# Description		: Build script for this directory.
+# Primary Authod	: Akash Kollipara [akashkollipara@gmail.com]
+#
+
 AVR_ARCH_DIR	:= $(GET_PATH)
 
 $(eval $(call add_include,$(AVR_ARCH_DIR)/include/))
