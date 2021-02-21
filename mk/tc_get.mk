aa7588dc5378e934a489634316e855bcfe1c673d
Akash Kollipara <akashkollipara@gmail.com>
2021-02-21 11:01:41 +0530

    <TC> Removed toolchain get path * User's/developer's can provide own git path for toolchain

--- mk/tc_get.mk
+++ mk/tc_get.mk
@@ -2,9 +2,9 @@ include mk/path.mk
 
 T_ALLOWLIST	+= get_all_tc get_avr_tc get_arm_tc get_riscv_tc
 
-AVR_TC_REPO	:= git@github.com:akashkollipara/avr-toolchain.git -b linux_x64_v5.4.0
-RISC_V_TC_REPO	:= git@github.com:akashkollipara/risc-v-toolchain.git -b linux_x64_v8.2.0
-ARM_TC_REPO	:= git@github.com:akashkollipara/arm-toolchain.git -b linux_x64_v7.3.1
+AVR_TC_REPO	:=
+RISC_V_TC_REPO	:=
+ARM_TC_REPO	:=
 
 get_all_tc: --tc_clear get_avr_tc get_arm_tc get_riscv_tc
 
@@ -16,7 +16,7 @@ $(TOOLS_ROOT)/arm-toolchain/bin/arm-none-eabi-gcc:
 	$(info < ! > Fetching ARM Toolchain ...)
 	$(info < ? > Please be patient as this might take a while ...)
 	mkdir -p toolchain
-	cd $(TOOLS_ROOT); git clone $(ARM_TC_REPO); chmod +x -R $(@D)/..
+	cd $(TOOLS_ROOT); git clone $(ARM_TC_REPO) arm-toolchain; chmod +x -R $(@D)/..
 	echo "< / > Done !"
 
 get_avr_tc: $(TOOLS_ROOT)/avr-toolchain/bin/avr-gcc
@@ -24,7 +24,7 @@ $(TOOLS_ROOT)/avr-toolchain/bin/avr-gcc:
 	$(info < ! > Fetching AVR Toolchain ...)
 	$(info < ? > Please be patient as this might take a while ...)
 	mkdir -p toolchain
-	cd $(TOOLS_ROOT); git clone $(AVR_TC_REPO); chmod +x -R $(@D)/..
+	cd $(TOOLS_ROOT); git clone $(AVR_TC_REPO) avr-toolchain; chmod +x -R $(@D)/..
 	echo "< / > Done !"
 
 get_riscv_tc: $(TOOLS_ROOT)/risc-v-toolchain/bin/riscv64-unknown-elf-gcc
@@ -32,6 +32,6 @@ $(TOOLS_ROOT)/risc-v-toolchain/bin/riscv64-unknown-elf-gcc:
 	$(info < ! > Fetching RISC-V Toolchain ...)
 	$(info < ? > Please be patient as this might take a while ...)
 	mkdir -p toolchain
-	cd $(TOOLS_ROOT); git clone $(RISC_V_TC_REPO); chmod +x -R $(@D)/..
+	cd $(TOOLS_ROOT); git clone $(RISC_V_TC_REPO) risc-v-toolchain; chmod +x -R $(@D)/..
 	echo "< / > Done !"
 
