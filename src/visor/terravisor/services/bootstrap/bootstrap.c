6a98c9a6dc0a0d5637785f4922d9ec50fdd7fb5e
Akash Kollipara <akashkollipara@gmail.com>
2021-03-09 16:13:01 +0530

    name fix

--- src/visor/terravisor/services/bootstrap/bootstrap.c
+++ src/visor/terravisor/services/bootstrap/bootstrap.c
@@ -2,7 +2,7 @@
  * CYANCORE LICENSE
  * Copyrights (C) 2019, Cyancore Team
  *
- * File Name		: machine_call.c
+ * File Name		: bootstrap.c
  * Description		: This file contains sources of bootstrap routine
  * Primary Author	: Akash Kollipara [akashkollipara@gmail.com]
  * Organisation		: Cyancore Core-Team
