99890f206da7ab4330707988cbb1b2c302d48b30
Akash Kollipara <akashkollipara@gmail.com>
2021-03-09 15:04:37 +0530

    <LibNMath> added file headers

--- src/lib/libnmath/nmath.c
+++ src/lib/libnmath/nmath.c
@@ -1,3 +1,13 @@
+/*
+ * CYANCORE LICENSE
+ * Copyrights (C) 2019, Cyancore Team
+ *
+ * File Name		: nmath.c
+ * Description		: This file contains sources of neo-math function
+ * Primary Author	: Akash Kollipara [akashkollipara@gmail.com]
+ * Organisation		: Cyancore Core-Team
+ */
+
 #include <nmath.h>
 
 unsigned int clog2(unsigned long num)
