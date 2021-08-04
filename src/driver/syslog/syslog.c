ed0335f4ae9afb1fddabe0e06603be03db35513d
Pranjal <pranjalchanda08@gmail.com>
2021-08-04 09:42:31 +0530

    + <Driver> Syslog code commit Ready for review.

--- src/driver/syslog/syslog.c
+++ src/driver/syslog/syslog.c
@@ -2,8 +2,8 @@
  * CYANCORE LICENSE
  * Copyrights (C) 2019, Cyancore Team
  *
- * File Name		: syslog.h
- * Description		: This file defines the syscalls for system console logging
+ * File Name		: syslog.c
+ * Description		: This file is the source for the syscalls of system console logging
  * Primary Author	: Pranjal Chanda [pranjalchanda08@gmail.com]
  * Organisation		: Cyancore Core-Team
  */
@@ -15,13 +15,13 @@ const char console_name = "earlycon"
 
 status_t syslog_setup(syslog_level_t sys_log_level)
 {
-	CHECK_ARGS(g_syslog.attach != SYSLOG_ATTACHED);
-	CHECK_ARGS((sys_log_level < syslog_level_max) && (sys_log_level > syslog_level_min));
+	RET_ERR(g_syslog.attach != SYSLOG_ATTACHED, error_init_done);
+	RET_ERR((sys_log_level < syslog_level_max) && (sys_log_level > syslog_level_min), error_inval_arg);
 
 	status_t ret = success;
 
 	ret = driver_setup(console_name);
-	if (ret == success)
+	if (ret == success || ret == error_init_done)
 	{
 		g_syslog.attach     = SYSLOG_ATTACHED;
 		g_syslog.syslog_fmt = SYSLOG_FORMAT;
@@ -31,53 +31,53 @@ status_t syslog_setup(syslog_level_t sys_log_level)
 }
 status_t syslog_set_level(syslog_level_t sys_log_level)
 {
-	CHECK_ARGS(g_syslog.attach == SYSLOG_ATTACHED);
-	CHECK_ARGS((sys_log_level < syslog_level_max) && (sys_log_level > syslog_level_min));
+	RET_ERR(g_syslog.attach == SYSLOG_ATTACHED, error_init_not_done);
+	RET_ERR((sys_log_level < syslog_level_max) && (sys_log_level > syslog_level_min), error_inval_arg);
 
 	g_syslog.sys_log_level = sys_log_level;
 	return success;
 }
 status_t syslog_get_level(syslog_level_t *sys_log_level)
 {
-	CHECK_ARGS(g_syslog.attach == SYSLOG_ATTACHED);
+	RET_ERR(g_syslog.attach == SYSLOG_ATTACHED, error_init_not_done);
 	*sys_log_level = g_syslog.sys_log_level;
 	return success;
 }
-status_t syslog_log(const char *agent, const char *output_str, syslog_level_t syslog_level)
+status_t syslog_log(const char *agent, const char *output_str, syslog_level_t log_level)
 {
-	CHECK_ARGS(g_syslog.attach == SYSLOG_ATTACHED);
-	CHECK_ARGS((sys_log_level < syslog_level_max) && (sys_log_level > syslog_level_min));
+	RET_ERR(g_syslog.attach == SYSLOG_ATTACHED, error_init_not_done);
+	RET_ERR((sys_log_level < syslog_level_max) && (sys_log_level > syslog_level_min), error_inval_arg);
 
-	if (syslog_level >= g_syslog.sys_log_level)
+	if (log_level >= g_syslog.sys_log_level)
 	{
-		const char * log_level;
-		switch (syslog_level)
+		const char * log_level_str;
+		switch (log_level)
 		{
 			case syslog_level_debug:
-				log_level = SYSLOG_DEBUG;
+				log_level_str = SYSLOG_DEBUG;
 				break;
 			case syslog_level_info:
-				log_level = SYSLOG_INFO;
+				log_level_str = SYSLOG_INFO;
 				break;
 			case syslog_level_err:
-				log_level = SYSLOG_ERR;
+				log_level_str = SYSLOG_ERR;
 				break;
 			case syslog_level_warn:
-				log_level = SYSLOG_WARN;
+				log_level_str = SYSLOG_WARN;
 				break;
 			case syslog_level_critical:
-				log_level = SYSLOG_CRITICAL;
+				log_level_str = SYSLOG_CRITICAL;
 				break;
 			default:
 				return error_inval_arg;
 		}
 
-		printf(g_syslog.syslog_fmt, agent, output_str, log_level);
+		printf(g_syslog.syslog_fmt, agent, output_str, log_level_str);
 	}
 }
 status_t syslog_release(void)
 {
-	CHECK_ARGS(g_syslog.attach == SYSLOG_ATTACHED);
+	RET_ERR(g_syslog.attach == SYSLOG_ATTACHED, error_init_not_done);
 
 	driver_exit(console_name);
 	memset(g_syslog, 0x00, sizeof(g_syslog));
--- src/include/syslog/syslog.h
+++ src/include/syslog/syslog.h
@@ -25,9 +25,9 @@
 
 #define SYSLOG_ATTACHED		0x1425
 
-#define CHECK_ARGS(x)		{ if(x){ (void)0; } else return error_inval_arg;}
+#define RET_ERR(x, err)		{ if(x){ (void)0; } else return err;}
 
-typedef enum
+typedef enum syslog_level
 {
 	syslog_level_min	= 0,
 	syslog_level_debug	= 10,
@@ -45,8 +45,81 @@ typedef struct syslog
 	syslog_level_t sys_log_level;
 } syslog_t;
 
+/**
+ * syslog_setup - Initialize the logging system
+ * This function is to setup the system logging module to be used by all the levels of the system
+ *
+ * @in		sys_log_level - This is the initial logging level of the logging system.
+ *
+ * @return	status:
+ * 		success
+ * 		error_inval_arg		for argument errors
+ * 		error_init_done		if the initialisation is already done
+ */
 status_t syslog_setup(syslog_level_t sys_log_level);
+
+/**
+ * syslog_set_level - Set new logging level for system logging
+ * This function helps to set a new level of system logging
+ * Logging levels available:
+ * 	1. syslog_level_debug (minimum)
+ *	2. syslog_level_info
+ *	3. syslog_level_warn
+ *	4. syslog_level_err
+ *	5. syslog_level_critical (maximum)
+ *
+ * @in		sys_log_level - This is the new logging level of the logging system.
+ *
+ * @return	status:
+ * 		success
+ * 		error_inval_arg		for argument errors
+ * 		error_init_not_done	if the initialisation is not done
+ */
 status_t syslog_set_level(syslog_level_t sys_log_level);
+
+/**
+ * syslog_get_level - Get the current logging level for system logging
+ * This function helps to get the current level of system logging
+ *
+ * @out 	sys_log_level - This is the current logging level of the logging system.
+ *
+ * @return	status:
+ * 		success
+ * 		error_inval_arg		for argument errors
+ * 		error_init_not_done	if the initialisation is not done
+ */
 status_t syslog_get_level(syslog_level_t * sys_log_level);
-status_t syslog_log(const char * agent, const char * output_str, syslog_level_t syslog_level);
+
+/**
+ * syslog_log - Produce log
+ * This function helps to create a log on the console output.
+ * Logging levels available:
+ * 	1. syslog_level_debug (minimum)
+ *	2. syslog_level_info
+ *	3. syslog_level_warn
+ *	4. syslog_level_err
+ *	5. syslog_level_critical (maximum)
+ *
+ * @in		agent - Pointer to the agent string (limit: 15 char)
+ * @in		output_str - string that is to be part of the information
+ * @in		log_level - Level of the output log (Ref: Logging levels available)
+ *
+ * @return	status:
+ * 		success
+ * 		error_inval_arg		for argument errors
+ * 		error_init_not_done	if the initialisation is not done
+ */
+status_t syslog_log(const char * agent, const char * output_str, syslog_level_t log_level);
+
+/**
+ * syslog_release - Release syslog module
+ * This function helps to release the syslog module
+ *
+ * @in/out	void
+ *
+ * @return	status:
+ * 		success
+ * 		error_inval_arg		for argument errors
+ * 		error_init_not_done	if the initialisation is not done
+ */
 status_t syslog_release(void);
