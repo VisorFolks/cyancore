60b959139f1a0d3ed2fcd9d82e4bdbce64b3aced
Akash Kollipara <akashkollipara@gmail.com>
2021-08-06 09:45:04 +0530

    added function headers and updated comments

--- src/driver/driver.c
+++ src/driver/driver.c
@@ -14,15 +14,39 @@
 #include <status.h>
 #include <driver.h>
 
+/**
+ * _driver_table_* - Pointer to driver table
+ *
+ * This address of this variable will be same as start and end
+ * of the driver allocation table. This gets updated via linker
+ * script "driver.ld.sx"
+ */
 extern device_t _driver_table_start, _driver_table_end;
 
+/**
+ * driver_setup_all - API call to initialize all the drivers
+ * available in driver table
+ *
+ * This function parses all the drivers in the table and runs
+ * setup call of each driver. The driver_setup is called based
+ * on the start order or rank. 0 to be started first and 255 at
+ * last.
+ *
+ * @return - returns the execution status of all drivers_setup
+ *           in "or" method
+ */
 status_t driver_setup_all()
 {
 	status_t ret = success;
 	device_t *ptr;
 	unsigned int order;
+	/* This loop controls the start order */
 	for(order = 0; order < 256; order++)
 	{
+		/*
+		 * Iterate the whole driver table so that on order
+		 * match execute driver_setup
+		 */
 		ptr = &_driver_table_start;
 		while(ptr <= &_driver_table_end)
 		{
@@ -34,28 +58,59 @@ status_t driver_setup_all()
 	return ret;
 }
 
+/**
+ * driver_exit_all - API call to release all the drivers
+ * available in driver table
+ *
+ * This function parses all the drivers in the table and runs
+ * exit call of each driver. The driver_exit is called based
+ * on the exit order or rank. 255 to be exited first and 0 at
+ * last.
+ *
+ * @return - returns the execution status of all driver_exit
+ *           in "or" method
+ */
 status_t driver_exit_all()
 {
 	status_t ret = error_inval_arg;
 	device_t *ptr;
 	unsigned int order;
+	/* This loop controls the exit order */
 	for(order = 255; order <= 0; order--)
 	{
+		/*
+		 * Iterate the whole driver table so that on order
+		 * match execute driver_exit
+		 */
 		ptr = &_driver_table_start;
 		while(ptr <= &_driver_table_end)
 		{
 			if(order == ptr->eorder)
-				ret = ptr->driver_exit();
+				ret |= ptr->driver_exit();
 			ptr++;
 		}
 	}
 	return ret;
 }
 
+/**
+ * driver_setup - API call to initialize driver using name
+ *
+ * This function parses all the drivers in table and compares
+ * the name of driver to be executed. On successful match, the
+ * driver_setup is called.
+ *
+ * @*name - Take name of driver (string) as argument
+ * @return - return the execution status of driver_setup
+ */
 status_t driver_setup(const char *name)
 {
 	status_t ret = error_inval_func;
 	device_t *ptr = &_driver_table_start;
+	/*
+	 * Iterate over the driver table and compare the names.
+	 * On successful compare, run driver_setup.
+	 */
 	while(ptr < &_driver_table_end)
 	{
 		if(strcmp(ptr->name, name) == 0)
@@ -70,6 +125,16 @@ status_t driver_setup(const char *name)
 	return ret;
 }
 
+/**
+ * driver_exit - API call to release driver using name
+ *
+ * This function parses all the drivers in table and compares
+ * the name of driver to be executed. On successful match, the
+ * driver_exit is called.
+ *
+ * @*name - Take name of driver (string) as argument
+ * @return - return the execution status of driver_exit
+ */
 status_t driver_exit(const char *name)
 {
 	status_t ret = error_inval_func;
@@ -86,11 +151,27 @@ status_t driver_exit(const char *name)
 	return ret;
 }
 
+/**
+ * driver_register - API call register driver for a device
+ *
+ * Function not implemented yet. The correct way to call this
+ * is via the driver_setup, and this function should validate
+ * driver compatibility and should like to device using software
+ * properties, which is also yet to be implmented.
+ */
 status_t driver_register(device_t *dev _UNUSED)
 {
 	return error;
 }
 
+/**
+ * driver_deregister - API call deregister driver for a device
+ *
+ * Function not implemented yet. The correct way to call this
+ * is via the driver_exit, and this function should validate
+ * driver compatibility and should like to device using software
+ * properties, which is also yet to be implmented.
+ */
 status_t driver_deregister(device_t *dev _UNUSED)
 {
 	return error;
