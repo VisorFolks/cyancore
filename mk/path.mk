44314aa5803c172168ec46e2dfe03815b3bb8933
Mayuri Lokhande <mayurilokhande01@gmail.com>
2021-10-14 16:11:26 +0530

    Bug fix: Replaced ARC with CC

--- mk/path.mk
+++ mk/path.mk
@@ -13,11 +13,11 @@
 #------------< Paths >-------------#
 #*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.#
 
-ARC_ROOT	:= .
-TOOLS_ROOT	?= $(abspath $(ARC_ROOT))/toolchain
+CC_ROOT		:= .
+TOOLS_ROOT	?= $(abspath $(CC_ROOT))/toolchain
 
-SRC		:= $(ARC_ROOT)/src
-OUT_PATH	?= $(ARC_ROOT)/out
+SRC		:= $(CC_ROOT)/src
+OUT_PATH	?= $(CC_ROOT)/out
 OUT		?= $(OUT_PATH)/$(PROJECT)
 
 GET_PATH	= $(patsubst %/,%,./$(dir $(lastword $(MAKEFILE_LIST))))
