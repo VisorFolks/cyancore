c090153f342f1c49fa14d16417b0e7d07bb84349
Akash Kollipara <akashkollipara@gmail.com>
2021-11-30 17:59:23 +0530

    <LibResource> FR in progress

--- src/lib/libresource/include/resource.h
+++ src/lib/libresource/include/resource.h
@@ -11,37 +11,5 @@
 
 #pragma once
 
-#include <dp/dp_system.h>
-#include <dp/dp_module.h>
-#include <dp/dp_gpio.h>
-
-typedef struct device_properties
-{
-	cpu_t *core[N_CORES];
-	uint8_t datawidth;
-	unsigned long base_clock;
-	memory_t *memory;
-#if GPIO == 1
-	gpio_module_t *port[N_PORT];
-#endif
-#if UART == 1
-	module_t *uart[N_UART];
-#endif
-#if ADC == 1
-	module_t *adc[N_ADC];
-#endif
-#if TIMER == 1
-	module_t *timer[N_TIMER];
-#endif
-#if SPI == 1
-	module_t *spi[N_SPI];
-#endif
-#if I2C == 1
-	module_t *i2c[N_I2C];
-#endif
-#if WDT == 1
-	module_t *wdt[N_WDT];
-#endif
-} dp_t;
-
-status_t dp_init(dp_t *);
+#include <dp.h>
+#include <sp.h>
