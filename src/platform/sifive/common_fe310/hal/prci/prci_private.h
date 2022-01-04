69bf04da494314a3f6a5c490e0e957b268ddbf4d
Akash Kollipara <akashkollipara@gmail.com>
2022-01-05 00:39:19 +0530

    <FE310> Add PLL apis

---
+++ src/platform/sifive/common_fe310/hal/prci/prci_private.h
@@ -0,0 +1,20 @@
+/*
+ * CYANCORE LICENSE
+ * Copyrights (C) 2019, Cyancore Team
+ *
+ * File Name		: prci_private.h
+ * Description		: This file contains defines of platform
+ *			  power, reset, clock and interrupt module
+ *			  register offsets
+ * Primary Author	: Akash Kollipara [akashkollipara@gmail.com]
+ * Organisation		: Cyancore Core-Team
+ */
+
+#pragma once
+
+/* PLL and Clock offsets */
+#define HFROSCCFG_OFFSET	0x00
+#define HFXOSCCFG_OFFSET	0x04
+#define PLLCFG_OFFSET		0x08
+#define PLLOUTDIV_OFFSET	0x0c
+#define PROCMONCFG_OFFSET	0xf0
