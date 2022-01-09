06bdbb66468f16fcdc0d8dd6a088174aed9bb74e
Pranjal Chanda <pranjalchanda08@gmail.com>
2022-01-09 21:52:32 +0530

    <POSIX> Add placeholders for undefined functions.

--- src/lib/posix/src/posix_time.c
+++ src/lib/posix/src/posix_time.c
@@ -10,6 +10,7 @@
 
 #include <lib/posix/include/time.h>
 #include <lib/posix/include/unistd.h>
+#include <lib/posix/include/errno.h>
 
 unsigned sleep( unsigned seconds )
 {
@@ -18,7 +19,7 @@ unsigned sleep( unsigned seconds )
 
 int usleep( useconds_t usec )
 {
-
+        TODO(usec);
 }
 
 clock_t clock( void )
@@ -28,17 +29,23 @@ clock_t clock( void )
 
 int clock_getcpuclockid( pid_t pid, clockid_t * clock_id )
 {
-
+        TODO(pid);
+        TODO(clock_id);
+        return SUCCESS;
 }
 
 int clock_getres( clockid_t clock_id, struct timespec * res )
 {
-
+        TODO(clock_id);
+        TODO(res);
+        return SUCCESS;
 }
 
 int clock_gettime( clockid_t clock_id, struct timespec * tp )
 {
-
+        TODO(clock_id);
+        TODO(tp);
+        return SUCCESS;
 }
 
 int clock_nanosleep( clockid_t clock_id,
@@ -46,42 +53,57 @@ int clock_nanosleep( clockid_t clock_id,
                      const struct timespec * rqtp,
                      struct timespec * rmtp )
 {
-
+        TODO(clock_id);
+        TODO(flags);
+        TODO(rqtp);
+        TODO(rmtp);
+        return SUCCESS;
 }
 
 int clock_settime( clockid_t clock_id,
                    const struct timespec * tp )
 {
-
+        TODO(clock_id);
+        TODO(tp);
+        return SUCCESS;
 }
 
 int nanosleep( const struct timespec * rqtp,
                struct timespec * rmtp )
 {
-
+        TODO(rqtp);
+        TODO(rmtp);
+        return SUCCESS;
 }
 
-int timer_create( clockid_t clockid,
+int timer_create( clockid_t clock_id,
                   struct sigevent * evp,
                   timer_t * timerid )
 {
-
+        TODO(clock_id);
+        TODO(evp);
+        TODO(timerid);
+        return SUCCESS;
 }
 
 int timer_delete( timer_t timerid )
 {
-
+        TODO(timerid);
+        return SUCCESS;
 }
 
 int timer_getoverrun( timer_t timerid )
 {
-
+        TODO(timerid);
+        return SUCCESS;
 }
 
 int timer_gettime( timer_t timerid,
                    struct itimerspec * value )
 {
-
+        TODO(timerid);
+        TODO(value);
+        return SUCCESS;
 }
 
 int timer_settime( timer_t timerid,
@@ -89,5 +111,9 @@ int timer_settime( timer_t timerid,
                    const struct itimerspec * value,
                    struct itimerspec * ovalue )
 {
-
+        TODO(timerid);
+        TODO(flags);
+        TODO(value);
+        TODO(ovalue);
+        return SUCCESS;
 }
