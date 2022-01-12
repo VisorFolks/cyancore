61e393cb2179d5c612b730797479b6336d4b7c30
Pranjal Chanda <pranjalchanda08@gmail.com>
2022-01-12 19:51:23 +0530

    <POSIX> Linting issue fixed

--- src/lib/build.mk
+++ src/lib/build.mk
@@ -14,7 +14,7 @@ LIB_DIR		:= $(GET_PATH)
 # Mandatory libraries
 include $(LIB_DIR)/libresource/build.mk
 include $(LIB_DIR)/libc/build.mk
-include $(LIB_DIR)/posix/build.mk
+include $(LIB_DIR)/libposix/build.mk
 #==================================================
 
 include $(LIB_DIR)/libnmath/build.mk
