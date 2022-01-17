f76aaec9897f03c2c9f4d4152fd1ce909e937524
Akash Kollipara <akashkollipara@gmail.com>
2022-01-18 01:43:53 +0530

    <Membuf> Removing console membuf
    - Membuf can always be earlycon

--- src/driver/console/console_membuf/build.mk
+++ src/driver/console/console_membuf/build.mk
@@ -11,8 +11,7 @@
 DIR		:= $(GET_PATH)
 
 $(eval $(call add_define,EARLYCON_MEMBUF))
-$(eval $(call add_define,CONSOLE_MEMBUF))
 
-ifeq ($(filter $(EARLYCON_MEMBUF) $(CONSOLE_MEMBUF),1),1)
+ifeq ($(EARLYCON_MEMBUF),1)
 include mk/obj.mk
 endif
