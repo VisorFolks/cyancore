74a46ccc3ddcab0e0936e349ed4d652c99803d4a
Mayuri Lokhande <mayurilokhande01@gmail.com>
2022-01-25 23:02:15 +0530

    <R2040> Add initial port for RPi pico

---
+++ src/projects/demo_rpipico/build.mk
@@ -0,0 +1,25 @@
+#
+# CYANCORE LICENSE
+# Copyrights (C) 2019, Cyancore Team
+#
+# File Name		: build.mk
+# Description		: This file build project sources and specifies
+#			  project properties
+# Primary Author	: Mayuri Lokhande [mayurilokhande01@gmail.com]
+# Organisation		: Cyancore Core-Team
+#
+
+PROJECT_DIR	:= $(GET_PATH)
+
+OPTIMIZATION	:= s
+
+EXE_MODE	:= supervisor
+
+include $(PROJECT_DIR)/config.mk
+
+# You can add another folder with necessary files to build the project
+# The below path should be valid
+# include $(PROJECT_DIR)/<folder>/build.mk
+
+DIR		:= $(PROJECT_DIR)
+include mk/obj.mk
