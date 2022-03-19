a06ff0f8e771dc4066b5dc540ed452931ef407b0
Akash Kollipara <akashkollipara@gmail.com>
2022-03-20 00:12:57 +0530

    <RV32I> Added arch irq save/restore calls

--- src/arch/riscv/32/i/terravisor/arch.c
+++ src/arch/riscv/32/i/terravisor/arch.c
@@ -79,12 +79,17 @@ void arch_setup2()
 	return;
 }
 
-void arch_di_save_state()
+void arch_di_save_state(istate_t *istate)
 {
+	istate_t temp;
+	asm volatile("csrr %0, mstatus" : "=r" (temp));
+	*istate = temp & (1 << 3 | 1 << 7);
+	arch_di();
 }
 
-void arch_ei_restore_state()
+void arch_ei_restore_state(istate_t *istate)
 {
+	asm volatile("csrs mstatus, %0" : : "r" (*istate));
 }
 
 /**
