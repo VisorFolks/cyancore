2a9174915707b544aec646681ddd8808dc6dbdc7
Akash Kollipara <akashkollipara@gmail.com>
2022-07-23 22:41:16 +0530

    <NMath> Add new functions to libnmath
    * Arithmetic
      - isPrime
    * Complex Math
      - new datatypes

--- src/lib/libnmath/arithmetic/build.mk
+++ src/lib/libnmath/arithmetic/build.mk
@@ -9,15 +9,5 @@
 # Organisation		: Cyancore Core-Team
 #
 
-NMATH_PATH	:= $(GET_PATH)
-LIB_OBJS	:=
-
-LIB		:= libnmath.a
-LIB_INCLUDE	+= $(NMATH_PATH)/include/
-DEP_LIBS_ARG	+= -lnmath
-
-include $(NMATH_PATH)/arithmetic/build.mk
-include $(NMATH_PATH)/dsp/build.mk
-
-DIR		:= $(NMATH_PATH)
-include mk/lib.mk
+DIR		:= $(GET_PATH)
+include mk/lobj.mk
