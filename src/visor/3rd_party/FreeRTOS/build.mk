11f5f216b35ffed7e86935e00ce4ca4f5256328d
Shashank Rangu <shashank.rangu@qbitlabs.co.in>
2022-08-07 21:42:59 +0530

    Initial Support for FreeRTOS

---
+++ src/visor/3rd_party/FreeRTOS/build.mk
@@ -0,0 +1,12 @@
+#
+# CYANCORE LICENSE
+# Copyrights (C) 2019, Cyancore Team
+#
+# File Name		: build.mk
+# Description		: Build script for this directory.
+# Primary Authod	: Shashank Rangu [shashank.r1996@gmail.com]
+# Organisation		: Cyancore Core-Team
+#
+
+DIR		:= $(GET_PATH)
+include mk/obj.mk
