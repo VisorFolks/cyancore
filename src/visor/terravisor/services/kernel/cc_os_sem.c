4b50c32bcb3de033810a9457d788f8f07d9c541a
Pranjal Chanda <pranjalchanda08@gmail.com>
2022-12-27 09:32:32 +0530

    <CC_OS> Semaphore complete
    !Need to add semaphore in ISR logic yet

--- src/visor/terravisor/services/kernel/cc_os_sem.c
+++ src/visor/terravisor/services/kernel/cc_os_sem.c
@@ -12,6 +12,7 @@
  *	INCLUDES
  *****************************************************/
 #include <terravisor/cc_os/cc_os_sem.h>
+#include <terravisor/cc_os/utils/cc_os_heap.h>
 
 /*****************************************************
  *	GLOBAL/STATIC VARIABLE DECLARATIONS
@@ -29,36 +30,71 @@
  *	USER FUNCTION DEFINATIONS
  *****************************************************/
 
-cc_os_err_t cc_os_sem_create	(sem_t * sem_ptr, size_t init_val)
+cc_os_err_t cc_os_sem_create	(sem_t ** sem_ptr, size_t init_val)
 {
-	CC_OS_ASSERT_IF_FALSE((sem_ptr != CC_OS_NULL_PTR && sem_ptr->sem_init == CC_OS_FALSE));
+#if CC_OS_DYNAMIC == CC_OS_FALSE
+	CC_OS_ASSERT_IF_FALSE((*sem_ptr != CC_OS_NULL_PTR && (*sem_ptr)->sem_init == CC_OS_FALSE));
 
-	sem_ptr->sem_init = CC_OS_TRUE;
-	sem_ptr->sem_val = init_val;
+	(*sem_ptr)->sem_init = CC_OS_TRUE;
+	(*sem_ptr)->sem_val = init_val;
+#else
+	CC_OS_ASSERT_IF_FALSE(*sem_ptr == CC_OS_NULL_PTR);
+	*sem_ptr = cc_os_malloc(sizeof(sem_t));
+	if (*sem_ptr == CC_OS_NULL_PTR)
+	{
+		return error_memory_low;
+	}
+	else
+	{
+		(*sem_ptr)->sem_val = init_val;
+		(*sem_ptr)->sem_init = CC_OS_TRUE;
+	}
 
+#endif
 	return success;
 }
-cc_os_err_t cc_os_sem_take 	(sem_t * sem_ptr)
+cc_os_err_t cc_os_sem_take 	(sem_t * sem_ptr, size_t wait_ticks)
 {
 	CC_OS_ASSERT_IF_FALSE((sem_ptr != CC_OS_NULL_PTR && sem_ptr->sem_init != CC_OS_FALSE));
 
+	if (sem_ptr->sem_val == CC_OS_FALSE)
+	{
+		if (wait_ticks == CC_OS_FALSE) 	/* ||_IS_ISR */
+		{
+			return error_os_sem_get;
+		}
+		else
+		{
+			cc_os_task_wait(wait_ticks);
+		}
+	}
+	else
+	{
+		sem_ptr->sem_val--;
+	}
 	return success;
 }
-cc_os_err_t cc_os_sem_give 	(sem_t * sem_ptr)
+cc_os_err_t cc_os_sem_give (sem_t * sem_ptr)
 {
 	CC_OS_ASSERT_IF_FALSE((sem_ptr != CC_OS_NULL_PTR && sem_ptr->sem_init != CC_OS_FALSE));
 
+	sem_ptr->sem_val++;
+
 	return success;
 }
-cc_os_err_t cc_os_sem_delete 	(sem_t * sem_ptr)
+cc_os_err_t cc_os_sem_delete (sem_t ** sem_ptr)
 {
-	CC_OS_ASSERT_IF_FALSE((sem_ptr != CC_OS_NULL_PTR && sem_ptr->sem_init != CC_OS_FALSE));
+	CC_OS_ASSERT_IF_FALSE((*sem_ptr != CC_OS_NULL_PTR && (*sem_ptr)->sem_init != CC_OS_FALSE));
 
-	sem_ptr->sem_init = CC_OS_FALSE;
+	(*sem_ptr)->sem_init = CC_OS_FALSE;
+
+#if CC_OS_DYNAMIC == CC_OS_TRUE
+	cc_os_free(*sem_ptr);
+#endif
 
 	return success;
 }
-cc_os_err_t cc_os_sem_get_val 	(sem_t * sem_ptr, size_t * val)
+cc_os_err_t cc_os_sem_get_val 	(const sem_t * sem_ptr, size_t * val)
 {
 	CC_OS_ASSERT_IF_FALSE((sem_ptr != CC_OS_NULL_PTR && sem_ptr->sem_init != CC_OS_FALSE));
 	CC_OS_ASSERT_IF_FALSE(val != CC_OS_NULL_PTR);
