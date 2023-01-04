444972dbc94ff8f5fc1eab5562f58175d44d37bb
Pranjal Chanda <pranjalchanda08@gmail.com>
2023-01-04 20:59:14 +0530

    <CC_OS> Test Application created
    + avr
    + riscv

---
+++ projects/demo_cc_os_avr/config.mk
@@ -0,0 +1,24 @@
+#
+# CYANCORE LICENSE
+# Copyrights (C) 2019-2023, Cyancore Team
+#
+# File Name		: config.mk
+# Description		: This file consists of project config
+# Primary Author	: Pranjal Chanda [pranjalchanda08@gmail.com]
+# Organisation		: Cyancore Core-Team
+#
+
+COMPILER	:= gcc
+TC_VER		:= 5.4.0
+FAMILY		:= mega_avr
+PLATFORM	:= atmega328p
+HEAP_SIZE 	:= 256
+STACK_SIZE 	:= 256
+STDLOG_MEMBUF	:= 0
+BOOTMSGS	:= 0
+EARLYCON_SERIAL	:= 0
+CONSOLE_SERIAL	:= 0
+OBRDLED_ENABLE	:= 1
+TERRAKERN	:= 1
+
+include $(PROJECT_DIR)/cc_os_config.mk
