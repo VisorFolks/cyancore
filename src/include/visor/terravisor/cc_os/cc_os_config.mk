053947d5dcf637b19ac98d80cf28fcd0fb54ebf1
Pranjal Chanda <pranjalchanda08@gmail.com>
2023-01-05 10:52:22 +0530

    <CC_OS> Review Comments

--- src/include/visor/terravisor/cc_os/cc_os_config.mk
+++ src/include/visor/terravisor/cc_os/cc_os_config.mk
@@ -8,14 +8,14 @@
 # Organisation		: Cyancore Core-Team
 #
 
-ccosconfig_CC_OS_USE_DYNAMIC 		?= 0
-ccosconfig_CC_OS_HEAP_SIZE 		?= 1024
-ccosconfig_CC_OS_MAX_THREAD 		?= 2
-ccosconfig_CC_OS_TASK_STACK_LEN 	?= 255
-ccosconfig_CC_OS_POWER_SAVE_EN 		?= 0
+CC_OS_USE_DYNAMIC 		?= 0
+CC_OS_HEAP_SIZE 		?= 1024
+CC_OS_MAX_THREAD 		?= 2
+CC_OS_TASK_STACK_LEN 	?= 255
+CC_OS_POWER_SAVE_EN 		?= 0
 
-$(eval $(call add_define,ccosconfig_CC_OS_USE_DYNAMIC))
-$(eval $(call add_define,ccosconfig_CC_OS_HEAP_SIZE))
-$(eval $(call add_define,ccosconfig_CC_OS_MAX_THREAD))
-$(eval $(call add_define,ccosconfig_CC_OS_TASK_STACK_LEN))
-$(eval $(call add_define,ccosconfig_CC_OS_POWER_SAVE_EN))
+$(eval $(call add_define,CC_OS_USE_DYNAMIC))
+$(eval $(call add_define,CC_OS_HEAP_SIZE))
+$(eval $(call add_define,CC_OS_MAX_THREAD))
+$(eval $(call add_define,CC_OS_TASK_STACK_LEN))
+$(eval $(call add_define,CC_OS_POWER_SAVE_EN))
