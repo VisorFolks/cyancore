77c0d3ad5730d76550ddb28eb8339dfb005b5f85
Akash Kollipara <akashkollipara@gmail.com>
2023-01-13 22:22:17 +0530

    <AVR8> Add support for floating point support
    Note: Added -lm (libmath) at the end of compilation as it support
    soft-fp ops. This may lead to future compilation issue when
    math functions are implemented.

--- mk/elf.mk
+++ mk/elf.mk
@@ -26,7 +26,7 @@ elf: $(ELF)
 $(ELF): $(DEP_LIBS) $(DEP_OBJS) $(LD_SCRIPT) $(LD_SUPPLEMENT) | $$(SIZE)
 	@echo "Elf: Generating $(@F) ..."
 	$(LD) -dT $(LD_SCRIPT) $(addprefix -T , $(LD_SUPPLEMENT)) $(LD_FLAGS)	\
-	-Map=$(@:.elf=.map) -o $@ $(filter %.o, $^) $(DEP_LIB_PATH) $(DEP_LIBS_ARG) -L $(TL) -lgcc
+	-Map=$(@:.elf=.map) -o $@ $(filter %.o, $^) $(DEP_LIB_PATH) $(DEP_LIBS_ARG) $(EXLIB_ARGS) -L $(TL) -lgcc
 	$(OD) -Dx -h --wide $@ > $(@:.elf=.lst)
 	$(OC) -O binary $@ $(@:.elf=.bin)
 	$(OC) -O ihex $@ $(@:.elf=.hex)
