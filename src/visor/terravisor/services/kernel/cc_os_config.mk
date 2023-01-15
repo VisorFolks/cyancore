4d86f81e63bf23d10bbe9e49f7f1f9637b2333c2
Pranjal Chanda <40349163+pranjalchanda08@users.noreply.github.com>
2023-01-15 15:18:01 +0530

    <CC_OS> - cc_os_config.h done by cc_os_config.mk (#208)
    Signed-off-by: panjalchanda08 <pranjalchanda08@gmail.com>

--- src/visor/terravisor/services/kernel/cc_os_config.mk
+++ src/visor/terravisor/services/kernel/cc_os_config.mk
@@ -11,11 +11,15 @@
 CC_OS_USE_DYNAMIC 		?= 1
 CC_OS_HEAP_SIZE 		?= 1024
 CC_OS_MAX_THREAD 		?= 2
+CC_OS_IDLE_TASK_PRIORITY	:= 1
+CC_OS_IDLE_TASK_NAME		?= \"CC_OS_IDLE\"
 CC_OS_IDLE_TASK_STACK_LEN 	?= 255
 CC_OS_POWER_SAVE_EN 		?= 0
 
 $(eval $(call add_define,CC_OS_USE_DYNAMIC))
 $(eval $(call add_define,CC_OS_HEAP_SIZE))
 $(eval $(call add_define,CC_OS_MAX_THREAD))
+$(eval $(call add_define,CC_OS_IDLE_TASK_NAME))
+$(eval $(call add_define,CC_OS_IDLE_TASK_PRIORITY))
 $(eval $(call add_define,CC_OS_IDLE_TASK_STACK_LEN))
 $(eval $(call add_define,CC_OS_POWER_SAVE_EN))
