397fc09c0b24cf8d24dcb5b0b05b180564df297b
Pranjal Chanda <pranjalchanda08@gmail.com>
2023-01-31 23:33:17 +0530

    <HELIOS> CC_OS -> HELIOS

--- src/include/visor/terravisor/helios/helios_sem.h
+++ src/include/visor/terravisor/helios/helios_sem.h
@@ -2,7 +2,7 @@
  * CYANCORE LICENSE
  * Copyrights (C) 2022, Cyancore Team
  *
- * File Name		: cc_os_sem.h
+ * File Name		: helios_sem.h
  * Description		: CC OS semaphore declaration
  * Primary Author	: Pranjal Chanda [pranjalchanda08@gmail.com]
  * Organisation		: Cyancore Core-Team
@@ -12,7 +12,7 @@
 /*****************************************************
  *	DEFINES
  *****************************************************/
-#include <terravisor/cc_os/cc_os.h>
+#include <terravisor/helios/helios.h>
 /*****************************************************
  *	TYPEDEFS
  *****************************************************/
@@ -26,7 +26,7 @@ typedef struct sem
  *****************************************************/
 #define CC_SEM_INST(_Name)	_Name##_sem_inst
 
-#if CC_OS_DYNAMIC == false
+#if HELIOS_DYNAMIC == false
 #define CC_SEM_DEF(_Name)	\
 static sem_t _Name##_sem = {	\
 	.sem_init = 0,		\
@@ -35,7 +35,7 @@ static sem_t _Name##_sem = {	\
 sem_t * _Name##_sem_inst = &_Name##_sem
 #else
 #define CC_SEM_DEF(_Name)	\
-sem_t * _Name##_sem_inst = CC_OS_NULL_PTR
+sem_t * _Name##_sem_inst = HELIOS_NULL_PTR
 #endif
 /*****************************************************
  *	USER FUNCTION DECLARATIONS
@@ -49,7 +49,7 @@ sem_t * _Name##_sem_inst = CC_OS_NULL_PTR
  *
  * @return status_t
  */
-status_t cc_os_sem_create	(sem_t ** sem_ptr, size_t init_val);
+status_t helios_sem_create	(sem_t ** sem_ptr, size_t init_val);
 
 /**
  * @brief 	Delete a semaphore and de-initialise it
@@ -58,7 +58,7 @@ status_t cc_os_sem_create	(sem_t ** sem_ptr, size_t init_val);
  *
  * @return status_t
  */
-status_t cc_os_sem_delete 	(sem_t ** sem_ptr);
+status_t helios_sem_delete 	(sem_t ** sem_ptr);
 
 /**
  * @brief 	Decrement a semaphore value
@@ -67,7 +67,7 @@ status_t cc_os_sem_delete 	(sem_t ** sem_ptr);
  *
  * @return status_t
  */
-status_t cc_os_sem_give 	(sem_t * sem_ptr);
+status_t helios_sem_give 	(sem_t * sem_ptr);
 
 /**
  * @brief 	Increment a semaphore value
@@ -77,7 +77,7 @@ status_t cc_os_sem_give 	(sem_t * sem_ptr);
  *
  * @return status_t
  */
-status_t cc_os_sem_take 	(sem_t * sem_ptr, size_t wait_ticks);
+status_t helios_sem_take 	(sem_t * sem_ptr, size_t wait_ticks);
 
 /**
  * @brief 	Get current semaphore value
@@ -88,4 +88,4 @@ status_t cc_os_sem_take 	(sem_t * sem_ptr, size_t wait_ticks);
  * @return status_t
  */
 
-status_t cc_os_sem_get_val 	(const sem_t * sem_ptr, size_t * val);
+status_t helios_sem_get_val 	(const sem_t * sem_ptr, size_t * val);
---
+++ src/include/visor/terravisor/helios/utils/helios_sched.h
@@ -0,0 +1,140 @@
+/*
+ * CYANCORE LICENSE
+ * Copyrights (C) 2022, Cyancore Team
+ *
+ * File Name		: helios_sched.h
+ * Description		: CC OS Kernel scheduler declaration
+ * Primary Author	: Pranjal Chanda [pranjalchanda08@gmail.com]
+ * Organisation		: Cyancore Core-Team
+ */
+
+#pragma once
+
+/*****************************************************
+ *	INCLUDES
+ *****************************************************/
+#include <stdint.h>
+#include <stdlib.h>
+#include <status.h>
+
+/*****************************************************
+ *	DEFINES
+ *****************************************************/
+#define HELIOS_NULL_PTR			NULL
+#define HELIOS_DELAY_MAX			SIZE_MAX
+
+#define	HELIOS_DYNAMIC 			HELIOS_USE_DYNAMIC
+
+#define HELIOS_ASSERT_IF_FALSE(con)	RET_ON_FAIL(con, error_func_inval_arg)
+
+/*****************************************************
+ *	TYPEDEFS
+ *****************************************************/
+typedef struct helios_sched_tcb helios_sched_tcb_t;
+typedef struct helios_sched helios_sched_t;
+typedef const char c_char;
+typedef void * helios_args;
+typedef void (*task_fn_t)(helios_args args);
+typedef void (*helios_cb_hook_t)(helios_args args);
+typedef enum
+{
+	helios_sched_task_status_exit	= 0x00,			///> Initial State
+	helios_sched_task_status_running	= 0x01,			///> Task currently running
+	helios_sched_task_status_ready	= 0x02,			///> Task Ready to despatch
+	helios_sched_task_status_wait	= 0x03,			///> Task in wait state
+	helios_sched_task_status_pause	= 0x04,			///> Task in pause state
+	helios_sched_task_status_max 	= 0xff,			///> Do Nt Use
+} helios_sched_task_status_t;
+
+/*
+ * @note	All negative callbacks are internally used by the scheduler
+ */
+typedef enum
+{
+	helios_sched_cb_power_pre_sleep	= 0x00,
+	helios_sched_cb_power_post_sleep	= 0x01,
+	helios_sched_cb_power_sleep		= 0x02,
+	helios_sched_cb_deadlock_notify	= 0x03,
+	helios_sched_cb_max			= 0xff
+}helios_sched_cb_t;
+
+typedef enum
+{
+	helios_task_flag_set_anti_deadlock	= (1 << 0)		///> Enable Antideadlock for the task
+}helios_task_flag_t;
+typedef struct link
+{
+	helios_sched_tcb_t * prev;
+	helios_sched_tcb_t * next;
+}link_t;
+
+typedef struct wres
+{
+	uintptr_t wait_on_resource;				///> Resource on hich the task is waiting on
+	size_t 	  task_delay_ticks;				///> Time delay in ticks
+}wres_t;
+
+struct helios_sched_tcb
+{
+
+	c_char  * name;						///> Name of the Current Task
+	uint8_t   priority;					///> Priority of the task
+	uint8_t	  task_flags;					///> Task Flags
+	uintptr_t stack_ptr;					///> Stack Pointer
+	task_fn_t task_func;					///> Task Call Function
+	uintptr_t args_ptr;					///> Task Call argument ptr
+	wres_t	  wait_res;					///> Wait Task resource
+	link_t	  ready_link;					///> Ready Linked List Pointers
+	link_t    wait_link;					///> Wait Linked List Pointers
+	uint16_t  task_id;					///> Task ID assigned
+#if HELIOS_ANTI_DEADLOCK
+	size_t	  task_wd_ticks;				///> Tick down counter for Anti Deadlock system
+#endif /* HELIOS_ANTI_DEADLOCK */
+	helios_sched_task_status_t task_status;			///> Current state of the task
+};
+
+typedef struct helios_sched_func_cb
+{
+	helios_cb_hook_t pre_sched;
+#if HELIOS_POWER_SAVE_EN
+	helios_cb_hook_t pre_sleep_cb;
+	helios_cb_hook_t post_sleep_cb;
+	helios_cb_hook_t sleep_cb;
+#endif /* HELIOS_POWER_SAVE_EN */
+#if HELIOS_ANTI_DEADLOCK
+	helios_cb_hook_t deadlock_notify;
+#endif /* HELIOS_ANTI_DEADLOCK */
+}helios_sched_func_cb_t;
+
+typedef struct helios_sched_ctrl
+{
+	helios_sched_tcb_t 	* ready_list_head;
+	helios_sched_tcb_t 	* curr_task;
+	helios_sched_tcb_t 	* wait_list_head;
+	helios_sched_t 	* selected_sched;
+	helios_sched_func_cb_t cb_hooks_reg;
+}helios_sched_ctrl_t;
+
+/**
+ * @brief Prototype of scheduler algorithm function
+ */
+typedef void (* algo_fn)(helios_sched_ctrl_t * sched_ctrl);
+
+typedef enum
+{
+	helios_sched_algo_round_robin	= 0x00,			///> Round Robin scheduling algorithm
+	helios_sched_algo_priority_driven	= 0x01,			///> Priority driven Scheduling
+	helios_sched_algo_max		= 0xff
+}helios_sched_algo_t;
+
+typedef struct helios_sched
+{
+	helios_sched_algo_t helios_selected_algo;			///> Selected Algorithm ID
+	algo_fn algo_function;					///> Pointer to algorithm function
+}helios_sched_t;
+
+typedef struct helios_sched_anti_deadlock
+{
+	c_char * name;
+	task_fn_t task_func;
+}helios_sched_anti_deadlock_t;
