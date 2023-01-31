49931b5a5184d14a8604cf6bbdaa790dd18a4e32
Pranjal Chanda <pranjalchanda08@gmail.com>
2023-01-31 23:33:17 +0530

    <HELIOS> CC_OS -> HELIOS

--- src/include/visor/terravisor/helios/utils/helios_heap.h
+++ src/include/visor/terravisor/helios/utils/helios_heap.h
@@ -2,7 +2,7 @@
  * CYANCORE LICENSE
  * Copyrights (C) 2022, Cyancore Team
  *
- * File Name		: cc_os_heap.h
+ * File Name		: helios_heap.h
  * Description		: CC OS Heap declaration (Placeholder)
  * Primary Author	: Pranjal Chanda [pranjalchanda08@gmail.com]
  * Organisation		: Cyancore Core-Team
@@ -11,7 +11,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
-#include <terravisor/cc_os/cc_os.h>
+#include <terravisor/helios/helios.h>
 
-void * cc_os_malloc(size_t size);
-void cc_os_free(void *addr);
+void * helios_malloc(size_t size);
+void helios_free(void *addr);
