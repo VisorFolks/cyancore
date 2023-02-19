6e0806e8c43f4c026bda3a18c8da1c78394177c1
Dybios <dibyajyoti.samal@gmail.com>
2023-02-19 19:14:44 +0100

    <HELIOS> Fix mutex logic
    Issue: #184
    
    - Fix lock and creation logic.
    - Use a definition instead for init_val.
    
    Signed-off-by: Dybios <dibyajyoti.samal@gmail.com>

--- src/visor/terravisor/services/kernel/helios_mutex.c
+++ src/visor/terravisor/services/kernel/helios_mutex.c
@@ -16,7 +16,7 @@
 /*****************************************************
  *	GLOBAL/STATIC VARIABLE DECLARATIONS
  *****************************************************/
-static const int init_val = 1;
+#define MUTEX_INIT_VAL 1
 
 /*****************************************************
  *	GLOBAL EXTERNS
@@ -37,9 +37,8 @@ status_t helios_mutex_create	(helios_mutex_t ** mutex_ptr)
 	HELIOS_ASSERT_IF_FALSE((*mutex_ptr)->mutex_init == false);
 
 	/* Initialize values of the mutex struct */
-	(*mutex_ptr)->lock_task = g_sched_ctrl.curr_task;
 	(*mutex_ptr)->mutex_init = true;
-	(*mutex_ptr)->mutex_val = init_val;
+	(*mutex_ptr)->mutex_val = MUTEX_INIT_VAL;
 #else
 	HELIOS_ASSERT_IF_FALSE(*mutex_ptr == HELIOS_NULL_PTR);
 	*mutex_ptr = helios_malloc(sizeof(helios_mutex_t));
@@ -51,8 +50,7 @@ status_t helios_mutex_create	(helios_mutex_t ** mutex_ptr)
 	else
 	{
 		/* Initialize values of the mutex struct */
-		(*mutex_ptr)->lock_task = g_sched_ctrl.curr_task;
-		(*mutex_ptr)->mutex_val = init_val;
+		(*mutex_ptr)->mutex_val = MUTEX_INIT_VAL;
 		(*mutex_ptr)->mutex_init = true;
 	}
 
@@ -67,7 +65,7 @@ status_t helios_mutex_lock 	(helios_mutex_t * mutex_ptr, size_t wait_ticks)
 
 	bool lock_flag = false;
 
-	if (mutex_ptr->mutex_val != init_val)
+	if ((mutex_ptr->mutex_val != MUTEX_INIT_VAL) && (mutex_ptr->lock_task != HELIOS_NULL_PTR))
 	{
 		if (wait_ticks == false) 	/* ||_IS_ISR */
 		{
@@ -76,27 +74,23 @@ status_t helios_mutex_lock 	(helios_mutex_t * mutex_ptr, size_t wait_ticks)
 		}
 		else
 		{
-			/* If not locked, check if another task has acquired the resource. */
+			/* If mutex is taken, lock_task must already hold info about the task. */
 			if (mutex_ptr->lock_task != g_sched_ctrl.curr_task) {
+				/* If locking task is not current task, wait for mutex */
 				g_sched_ctrl.curr_task->wait_res.wait_on_resource = (uintptr_t) mutex_ptr;
 				helios_task_wait(wait_ticks);
 			}
-			else {
-				/* If current task requests for lock again, allow locking recursively. */
+			else if (mutex_ptr->lock_task == g_sched_ctrl.curr_task) {
+				/* If locking task is current task, allow locking recursively */
 				lock_flag = true;
 			}
 		}
 	}
 	else
 	{
-		/* If no mutex acquired yet, only let the current task acquire lock. */
-		if (mutex_ptr->lock_task != g_sched_ctrl.curr_task) {
-			HELIOS_ERR("Mutex initialized by another task");
-			return error_os_mutex_lock;
-		}
-		else {
-			lock_flag = true;
-		}
+		/* Set locking task as current task and acquire lock. */
+		mutex_ptr->lock_task = g_sched_ctrl.curr_task;
+		lock_flag = true;
 	}
 
 	if (lock_flag) {
