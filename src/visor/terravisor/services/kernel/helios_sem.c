3bf689e0c172b711435b67094cebb5dc2429d9ea
Pranjal Chanda <pranjalchanda08@gmail.com>
2023-02-20 22:55:51 +0530

    <HELIOS> Sem Build fixes

--- src/visor/terravisor/services/kernel/helios_sem.c
+++ src/visor/terravisor/services/kernel/helios_sem.c
@@ -36,13 +36,15 @@ status_t helios_sem_create (helios_sem_t ** sem_ptr, size_t init_val)
 	HELIOS_ASSERT_IF_FALSE((*sem_ptr)->sem_init == false);
 #else
 	HELIOS_ASSERT_IF_FALSE(*sem_ptr == HELIOS_NULL_PTR);
+	/* Create semaphore */
 	*sem_ptr = helios_malloc(sizeof(helios_sem_t));
 	if (*sem_ptr == HELIOS_NULL_PTR)
 	{
 		HELIOS_ERR("Memory low for sem create");
 		return error_memory_low;
 	}
-#endif
+#endif 	/* HELIOS_DYNAMIC */
+	/* Fill up the init details */
 	(*sem_ptr)->sem_val = init_val;
 	(*sem_ptr)->sem_init = true;
 
@@ -54,19 +56,24 @@ status_t helios_sem_take (helios_sem_t * sem_ptr, size_t wait_ticks)
 	HELIOS_ASSERT_IF_FALSE(sem_ptr->sem_init != false);
 
 	if (sem_ptr->sem_val == false)
-	{
+	{	/* If sem already locked */
 		if (wait_ticks == false) 	/* ||_IS_ISR */
 		{
+			/* If Thread can not wait on resource to get unlocked */
 			HELIOS_ERR("Semaphore already locked");
 			return error_os_sem_get;
 		}
 		else
-		{	g_sched_ctrl.curr_task->wait_res.wait_on_resource = (uintptr_t) sem_ptr;
+		{
+			/* Set the TCB to point to locked resource */
+			g_sched_ctrl.curr_task->wait_res.wait_on_resource = (uintptr_t) sem_ptr;
+			/* Send to wait state */
 			helios_task_wait(wait_ticks);
 		}
 	}
 	else
 	{
+		/* Else Decrement semaphore value */
 		sem_ptr->sem_val--;
 	}
 	return success;
@@ -76,6 +83,7 @@ status_t helios_sem_give (helios_sem_t * sem_ptr)
 	HELIOS_ASSERT_IF_FALSE(sem_ptr != HELIOS_NULL_PTR);
 	HELIOS_ASSERT_IF_FALSE(sem_ptr->sem_init != false);
 
+	/* Increment Semaphore value */
 	sem_ptr->sem_val++;
 
 	return success;
@@ -83,11 +91,12 @@ status_t helios_sem_give (helios_sem_t * sem_ptr)
 status_t helios_sem_delete (helios_sem_t ** sem_ptr)
 {
 	HELIOS_ASSERT_IF_FALSE(sem_ptr != HELIOS_NULL_PTR);
-	HELIOS_ASSERT_IF_FALSE(sem_ptr->sem_init != false);
+	HELIOS_ASSERT_IF_FALSE((*sem_ptr)->sem_init != false);
 
 	(*sem_ptr)->sem_init = false;
 
 #if HELIOS_DYNAMIC == true
+	/* Free up allocated space */
 	helios_free(*sem_ptr);
 #endif
 
